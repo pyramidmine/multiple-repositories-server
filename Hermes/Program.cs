52a879e2399cf7c927a19cdb4c210da0b3646fbd
pyramidmine <pyramidmine@gmail.com>
2020-08-06 12:38:17 +0900

    최초 커밋

---
+++ Hermes/Program.cs
@@ -0,0 +1,17 @@
+﻿using MannaPlanet.Networks;
+using System;
+
+namespace MannaPlanet.Hermes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Networks.Person john = new Networks.Person { Name = "John", Age = 50 };
+            Console.WriteLine(john.GetInfo());
+
+            Networks.Person jane = new Networks.Person { Name = "Jane", Age = 0 };
+            Console.WriteLine(jane.GetInfo());
+        }
+    }
+}
